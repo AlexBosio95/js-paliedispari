90c64e28b3e967b0c1277a9bad2ee4ffda6fc085
AlexBosio95 <alex.bosio1995@gmail.com>
2022-06-22 16:45:30 +0200

    funzione evenAndOdd and randomNumber

--- js/script.js
+++ js/script.js
@@ -27,3 +27,53 @@ function reverseWord(parola) {
 
     return wordReverse;
 }
+
+
+// Pari e Dispari
+// L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
+// Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
+// Sommiamo i due numeri
+// Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
+// Dichiariamo chi ha vinto.
+
+const userSelect = prompt('scegli tra pari o dispari');
+const userNumberSelect = parseInt(prompt('scegli un numero'));
+const min = 1;
+const max = 5;
+const numerorandom = randomNumber(min, max);
+let risultato = numerorandom + userNumberSelect;
+let isEven = false;
+
+console.log('numero utente ' + userNumberSelect);
+console.log('numero pc random ' + numerorandom);
+console.log('somma = ' + risultato);
+
+evenAndOdd(risultato);
+
+if (userSelect == 'pari' && isEven == true){
+    console.log('Utente hai vinto il numero è pari');
+} else if (userSelect == 'dispari' && isEven == false) {
+    console.log('Utente hai vinto il numero è dispari');
+} else {
+    console.log('Ha vinto il PC')
+}
+
+
+
+
+// Funzione numero randomico
+function randomNumber(min, max) {
+    number = Math.floor(Math.random() * (max - min) + min)
+    return number
+}
+
+// Funzione pari e dispari
+function evenAndOdd(result){
+    if (result % 2 == 0){
+        console.log('il risultato è pari')
+        isEven = true;
+    } else {
+        console.log('il risultato è dispari')
+    }
+
+}
